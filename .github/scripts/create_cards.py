45de17efe7747cfeea90cdfe4c13b5be501dba09
jwj019 <justin.johnson5@nih.gov>
2023-07-06 19:29:24 -0400

    Get all cards to de-dupe

--- .github/scripts/create_cards.py
+++ .github/scripts/create_cards.py
@@ -18,5 +18,13 @@ with open(github_event, "r") as event_file:
 issue = event["issue"]
 if "security" in [label["name"] for label in issue["labels"]]:
     board = client.get_board(board_id)
-    list = board.list_lists()[list_index]  # assuming you want the first list
-    card = list.add_card(issue["title"], desc=issue["body"])
+    in_list = board.list_lists()[list_index]  # Get the list specified
+
+    # Get all cards in the board, including those in the archive
+    existing_cards = board.all_cards()
+
+    # Check if card already exists
+    if not any(card.name == issue["title"] for card in existing_cards):
+        card = in_list.add_card(issue["title"], desc=issue["body"])
+    else:
+        print("Card with title '{}' already exists.".format(issue["title"]))
