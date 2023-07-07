2972b535623076e2b8eb0610913aaef8c97ef987
jwj019 <justin.johnson5@nih.gov>
2023-07-07 05:08:35 -0400

    Update closed check & change trello link to commnt

--- .github/scripts/create_cards.py
+++ .github/scripts/create_cards.py
@@ -29,6 +29,7 @@ if "security" in [label["name"] for label in issue_data["labels"]]:
 
     # Get all cards in the board, including those in the archive
     existing_cards = board.all_cards()
+    archived_cards = board.closed_cards()
 
     issue_link = issue_data["html_url"]
     desc = f'{issue_data["body"]}\n\n[Link to GitHub Issue]({issue_link})'
@@ -37,20 +38,30 @@ if "security" in [label["name"] for label in issue_data["labels"]]:
     for card in existing_cards:
         if card.name == issue_data["title"]:
             # If the card is closed (archived), do nothing
-            if card.closed:
+            if card in archived_cards:
                 print("Card already closed.")
                 break
 
             print("Card already open. Updating description...")
             # Update the existing card
             card.set_description(desc)
-            # Update the GitHub issue with a link to the Trello card
-            issue.edit(body=f'{issue_data["body"]}\n\n[Trello Card]({card.url})')
+            trello_card_link = card.url
             break
     else:
         print("No card exists for this issue.  Creating new card...")
         # If no existing card is found, add a new card
         list = board.list_lists()[list_index]  # Put the card in the specified list
         card = list.add_card(issue_data["title"], desc=desc)
-        # Update the GitHub issue with a link to the Trello card
-        issue.edit(body=f'{issue_data["body"]}\n\n[Trello Card]({card.url})')
+        trello_card_link = card.url
+
+    # Add a comment to the GitHub issue with a link to the Trello card
+    repo = github_client.get_repo(issue_data["repository"]["full_name"])
+    issue = repo.get_issue(number=issue_data["number"])
+    comments = issue.get_comments()
+
+    # Check if a comment with a link to the Trello card already exists
+    for comment in comments:
+        if trello_card_link in comment.body:
+            break
+    else:
+        issue.create_comment(f"Related Trello card: {trello_card_link}")
